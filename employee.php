0cb57872c26d00d49ff202fe95628b41d0b90b1c
dipen <dipen.ec2010@gmail.com>
2016-10-10 21:48:29 +0530

    added nginx

--- employee.php
+++ employee.php
@@ -3,7 +3,7 @@
     $connection = mysqli_connect("localhost","root","123","employees") or die("Error " . mysqli_error($connection));
 
     //fetch table rows from mysql db
-    $sql = "select * from employees where gender='M' and birth_date = '1965-02-01' and hire_date > '1990-01-01'";
+    $sql = "select * from employees where gender='M' and birth_date = '1965-02-01' and hire_date > '1990-01-01' order by first_name";
     $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
     //create an array
